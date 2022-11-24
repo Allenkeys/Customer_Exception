e3987fe5df18c191e4206ec73d5cb46c85553455
Joshua Eze <ezejoshuac@gmail.com>
2022-11-24 10:13:20 +0100

    Add project files.

---
+++ Customer_Exception/NullStaffRegistrationException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custom_Exception
+{
+    public class NullStaffRegistrationException : Exception
+    {
+        public NullStaffRegistrationException() { }
+        public NullStaffRegistrationException(string message) : base(message) 
+        { }
+        public NullStaffRegistrationException(string message, Exception innerException) : base(message, innerException) 
+        { }
+    }
+}
